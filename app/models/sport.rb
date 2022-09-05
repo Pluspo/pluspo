585618bbaa8b8ee41e7aa0534d8a441747ecb1c0
yoshihiro-hy <hy07140714@gmail.com>
2022-09-05 13:07:17 +0900

    Fix: sportモデル・nameカラムバリデーション追加

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,5 +1,5 @@
 class Sport < ApplicationRecord
   has_many :areas_sports, dependent: :destroy
   has_many :sport_shedules, dependent: :destroy
-  validates :name, uniqueness:true
+  validates :name, presence: true, uniqueness:true
 end
