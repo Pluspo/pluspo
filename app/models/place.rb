3651ef842902da9a9ca7c92eb656a4c08435adeb
yoshihiro-hy <hy07140714@gmail.com>
2022-09-05 13:05:33 +0900

    Fix: placeモデル・各カラムバリデーション追加

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,7 @@
 class Place < ApplicationRecord
   has_many :areas, dependent: :destroy
+  validates :name, uniqueness:true
+  validates :opened_at, presence: true
+  validates :closed_at, presence: true
+  validates :address, presence: true
 end
