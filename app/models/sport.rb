33450b4b25c50897acadd689da08f3910ebb15e7
yoshihiro-hy <hy07140714@gmail.com>
2022-09-06 09:31:41 +0900

    Fix: sportモデルuniqueness: trueのスペース修正

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -2,5 +2,5 @@ class Sport < ApplicationRecord
   has_many :area_sports, dependent: :destroy
   has_many :areas, throught: :area_sports
   has_many :sport_shedules, dependent: :destroy
-  validates :name, presence: true, uniqueness:true
+  validates :name, presence: true, uniqueness: true
 end
