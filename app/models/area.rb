98038987ab6ca2cd9524d08c033699f048e5dfc2
yoshihiro-hy <hy07140714@gmail.com>
2022-09-06 10:38:20 +0900

    Fix: area・sportモデルdependent: :destroy記述修正

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,7 +1,9 @@
 class Area < ApplicationRecord
   belongs_to :place
-  has_many :area_sports, dependent: :destroy
-  has_many :sports, throught: :area_sports
+  with_options dependent: :destroy do
+    has_many :area_sports
+    has_many :sports, throught: :area_sports
+  end
   with_options presence: true do
     validates :name
     validates :opened_at
--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,6 +1,8 @@
 class Sport < ApplicationRecord
-  has_many :area_sports, dependent: :destroy
-  has_many :areas, throught: :area_sports
+  with_options dependent: :destroy do
+    has_many :area_sports
+    has_many :areas, throught: :area_sports
+  end
   has_many :sport_shedules, dependent: :destroy
   validates :name, presence: true, uniqueness: true
 end
