57c9ea378ce36ac96cb9cfad30ee6bbcebe785e1
yoshihiro-hy <hy07140714@gmail.com>
2022-09-06 09:08:35 +0900

    Fix: sportモデルとareaモデルの関連付け修正

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,5 +1,7 @@
 class Area < ApplicationRecord
   belongs_to :place
+  has_many :area_sports
+  has_many :sports, throught: :area_sports
   validates :name, presence: true
   validates :opened_at, presence: true
   validates :closed_at, presence: true
--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,5 +1,6 @@
 class Sport < ApplicationRecord
-  has_many :areas_sports, dependent: :destroy
+  has_many :area_sports
+  has_many :areas, throught: :area_sports
   has_many :sport_shedules, dependent: :destroy
   validates :name, presence: true, uniqueness:true
 end
