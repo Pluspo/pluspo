a6a9cb2d3541086136808d314d5ede253e7f973f
yoshihiro-hy <hy07140714@gmail.com>
2022-09-10 12:18:45 +0900

    Fix: sportモデル throught->through

--- app/models/shedule.rb
+++ app/models/shedule.rb
@@ -1,7 +1,6 @@
 class Shedule < ApplicationRecord
   belongs_to :area_sport
   with_options presence: true do
-    validates :cycles
     validates :started_at
     validates :finished_at
   end
--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,7 +1,7 @@
 class Sport < ApplicationRecord
   with_options dependent: :destroy do
     has_many :area_sports
-    has_many :areas, throught: :area_sports
+    has_many :areas, through: :area_sports
   end
   has_many :shedules, dependent: :destroy
   validates :name, presence: true, uniqueness: true
