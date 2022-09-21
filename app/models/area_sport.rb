8d72aebf55d902ef1807e89bd30d0bfa3daf4c0b
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2022-09-22 08:43:11 +0900

    Fix: AreaSportのhas_many :batchesを修正.

--- app/models/area_sport.rb
+++ app/models/area_sport.rb
@@ -22,5 +22,5 @@ class AreaSport < ApplicationRecord
   belongs_to :area
   belongs_to :sport
   has_many :schedules, dependent: :destroy
-  has_many :batches
+  has_many :batches, dependent: :destroy
 end
