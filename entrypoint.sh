f1e48ce45b1ba075031db758d10557d7107c595a
羽隅 喜輝 <66538632+yoshiki105@users.noreply.github.com>
2022-12-17 12:57:36 +0900

    fix: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # 自分のアプリに合わせて必要なコマンドを修正してください
-bin/rails db:migrate:reset
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:migrate:reset
 bin/rails db:seed_fu
 
 rm -f tmp/pids/server.pid && bin/rails s
