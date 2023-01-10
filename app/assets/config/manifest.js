638a61ae96231cd066c72b30df1e87c96b8f76eb
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2023-01-10 21:06:14 +0900

    Add: 追加でスタイルを修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,5 @@
+//= link_tree ../images
 //= link_tree ../../javascript/controllers
 //= link_tree ../../javascript/custom
-//= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link application.js
