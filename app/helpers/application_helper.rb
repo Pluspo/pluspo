17ab3000db87b663538b28fb1d70c34d9507230e
hisaju <hisaju@gmail.com>
2023-01-29 15:43:29 +0900

    HTMLと電話のオートリンク追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  PHONE_NUMBER_REGEXP = /\+?[ 0-9()-]{9,}[0-9)]/
+
+  def auto_phone_to(html)
+    html.gsub(PHONE_NUMBER_REGEXP) do |match|
+      phone_to(match)
+    end.html_safe
+  end
 end
