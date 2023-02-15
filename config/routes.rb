018e49bbf00e7f710b626b2a5647db73b8f3a3ae
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2023-02-15 20:40:42 +0900

    Add: その他のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root "places#index"
   resources :places, only: %i[index show]
+
+  get '/top', to: 'static_pages#top'
   get '/terms', to: 'static_pages#terms'
+  get '/privacy_policy', to: 'static_pages#privacy_policy'
+  get '/guide', to: 'static_pages#guide'
+  get '/faq', to: 'static_pages#faq'
 end
