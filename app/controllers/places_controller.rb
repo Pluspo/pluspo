3ffb3bd037cf36ac7ca15d16932cc3e5f64b7fd8
yoshihiro harada <hy07140714@gmail.com>
2023-03-17 11:46:57 +0900

    Fix: N+1問題を解決

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,11 +10,11 @@ class PlacesController < ApplicationController
 
   def set_place
     params[:id] ||= 1 # なんか違う気もする
-    @place = Place.find(params[:id]).decorate
+    @place = Place.includes(areas: [area_sports: :sport]).find(params[:id]).decorate
   end
 
   def set_schedules
     @q = Schedule.ransack(params[:q])
-    @schedules = @q.result.page(params[:page]).includes(area_sport: [:sport, { area: :place }]).order(started_at: :asc, finished_at: :asc).order("sport.name").after_today
+    @schedules = @q.result.includes(area_sport: [:sport, area: :place]).order(started_at: :asc, finished_at: :asc).order("sport.name").after_today.page(params[:page])
   end
 end
